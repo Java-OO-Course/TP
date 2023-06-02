4d1f1bd3bbeda892cc9830babc4b2b30e8892d6e
Valentin <valentin.bourcier@inria.fr>
2023-06-02 19:19:12 +0200

    Documentation de la correction du TP2
    commit 3bf9bc68ce727d5bbc24819d593a6b084d1fc15d
    Author: Valentin <valentin.bourcier@inria.fr>
    Date:   Fri Jun 2 19:17:03 2023 +0200
    
        docs: Ajout de commentaires sur les exercices du TP2
    
    commit a2abb751116bc342f18478a852c410b372dce97e
    Author: Valentin <valentin.bourcier@inria.fr>
    Date:   Fri Jun 2 17:26:13 2023 +0200
    
        feat: Q1.3, creation des "drawers"
    
    commit ba4574ab9df8f287ff67c89201539f8084ff81cd
    Author: Valentin <valentin.bourcier@inria.fr>
    Date:   Fri Jun 2 16:11:24 2023 +0200
    
        feat: Q1.2, correction  6, 7, 8
    
    commit b0916202f12a4623b571877be487cbaed0b216da
    Author: Valentin <valentin.bourcier@inria.fr>
    Date:   Wed May 31 14:49:18 2023 +0200
    
        feat: TP2 Q1.2.5, dessin des formes géométriques
    
        Le comportement de dessin est définit dans la classe GeometricShapeDrawer, méthode drawOn(Graphics g).
    
    commit 07d08a0ce8ac7951847c7dadccab572de2c04595
    Author: Valentin <valentin.bourcier@inria.fr>
    Date:   Wed May 31 12:46:08 2023 +0200
    
        refactor: TP2 Q1.2, introduction du Painter
    
        1. Création d'un package pour les formes géométriques.
        2. Création d'un package pour les classes du Painter.
        3. Création d'un package pour la classe GeometricShapeDrawer dont le besoin est amené par les classes du Painter.
    
    commit a87b902c3e50232186a0ab75178ae55f11b85019
    Author: Valentin <valentin.bourcier@inria.fr>
    Date:   Wed May 31 12:28:55 2023 +0200
    
        feat: TP2 Q1.1, ajout de GeometricShape et de ses sous-classes
    
        Lesdites sous-classes: Circle, Triangle, Rectangle et Square

--- src/main/java/cmo/lab/drawers/GeometricShapeDrawer.java
+++ src/main/java/cmo/lab/drawers/GeometricShapeDrawer.java
@@ -6,6 +6,10 @@ import java.awt.Graphics;
 
 public abstract class GeometricShapeDrawer {
 
+    /**
+     * La variable devra être castée/convertie en sous-classe de GeometricShape
+     * pour être utilisée par les sous-classes de GeometricShapeDrawer.
+     */
     protected GeometricShape shape;
 
     public GeometricShapeDrawer(GeometricShape aShape) {
